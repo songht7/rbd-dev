6fc95588951290da2e5c25d00ce1ce77d9b3a445
songht7 <407794660@qq.com>
2021-08-11 18:11:06 +0800

    xxxx

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="page-main">
+  <div id="Page-main">
     <b-row>
-      <div id="nav"
-        class="navbar-box"
-      >
-        <b-navbar type="dark" variant="dark">
+      <div id="nav" class="navbar-box">
+        <b-navbar type="light" variant="faded" fixed="top" :sticky="sticky">
           <b-container fluid="xl">
             <b-navbar-nav>
               <b-nav-item href="#">Home</b-nav-item>
@@ -25,133 +23,136 @@
         </b-navbar>
       </div>
     </b-row>
-
-    <b-container fluid="xl">
-      <div class="silde-box">
-        <b-carousel
-          id="carousel-1"
-          v-model="slide"
-          :interval="4000"
-          controls
-          indicators
-          background="#ababab"
-          img-width="1024"
-          img-height="480"
-          style="text-shadow: 1px 1px 2px #333"
-          @sliding-start="onSlideStart"
-          @sliding-end="onSlideEnd"
-        >
-          <!-- Text slides with image -->
-          <b-carousel-slide
-            caption="First slide"
-            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-            img-src="https://picsum.photos/1024/480/?image=52"
-          ></b-carousel-slide>
-
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Hello world!</h1>
-          </b-carousel-slide>
-
-          <!-- Slides with image only -->
-          <b-carousel-slide
-            img-src="https://picsum.photos/1024/480/?image=58"
-          ></b-carousel-slide>
-
-          <!-- Slides with img slot -->
-          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-          <b-carousel-slide>
-            <template v-slot:img>
-              <img
-                class="d-block img-fluid w-100"
-                width="1024"
-                height="480"
-                src="https://picsum.photos/1024/480/?image=55"
-                alt="image slot"
-              />
-            </template>
-          </b-carousel-slide>
-
-          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-          <b-carousel-slide
-            caption="Blank Image"
-            img-blank
-            img-alt="Blank image"
+    <div id="Page-container">
+      <b-container fluid="xl">
+        <div class="silde-box">
+          <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
           >
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse eros felis, tincidunt a tincidunt eget, convallis vel
-              est. Ut pellentesque ut lacus vel interdum.
-            </p>
-          </b-carousel-slide>
-        </b-carousel>
+            <!-- Text slides with image -->
+            <b-carousel-slide
+              caption="First slide"
+              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+              img-src="https://picsum.photos/1024/480/?image=52"
+            ></b-carousel-slide>
 
-        <p class="mt-4">
-          Slide #: {{ slide }}<br />
-          Sliding: {{ sliding }}
-        </p>
-      </div>
+            <!-- Slides with custom text -->
+            <b-carousel-slide
+              img-src="https://picsum.photos/1024/480/?image=54"
+            >
+              <h1>Hello world!</h1>
+            </b-carousel-slide>
 
-      <div class="jumbotron-box">
-        <b-row>
-          <b-jumbotron>
-            <template v-slot:header>BootstrapVue</template>
+            <!-- Slides with image only -->
+            <b-carousel-slide
+              img-src="https://picsum.photos/1024/480/?image=58"
+            ></b-carousel-slide>
 
-            <template v-slot:lead>
-              This is a simple hero unit, a simple jumbotron-style component for
-              calling extra attention to featured content or information.
-            </template>
+            <!-- Slides with img slot -->
+            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+            <b-carousel-slide>
+              <template v-slot:img>
+                <img
+                  class="d-block img-fluid w-100"
+                  width="1024"
+                  height="480"
+                  src="https://picsum.photos/1024/480/?image=55"
+                  alt="image slot"
+                />
+              </template>
+            </b-carousel-slide>
 
-            <hr class="my-4" />
+            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+            <b-carousel-slide
+              caption="Blank Image"
+              img-blank
+              img-alt="Blank image"
+            >
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse eros felis, tincidunt a tincidunt eget, convallis
+                vel est. Ut pellentesque ut lacus vel interdum.
+              </p>
+            </b-carousel-slide>
+          </b-carousel>
 
-            <p>
-              It uses utility classes for typography and spacing to space
-              content out within the larger container.
-            </p>
-
-            <b-button variant="primary" href="#">Do Something</b-button>
-            <b-button variant="success" href="#">Do Something Else</b-button>
-          </b-jumbotron>
-        </b-row>
-      </div>
-
-      <b-row>
-        <b-img
-          src="https://picsum.photos/1024/400/?image=41"
-          fluid
-          alt="Responsive image"
-        ></b-img>
-      </b-row>
-
-      <div class="form-box">
-        <b-container fluid>
-          <b-row class="my-1" v-for="type in types" :key="type">
-            <b-col sm="3">
-              <label :for="`type-${type}`"
-                >Type <code>{{ type }}</code
-                >:</label
-              >
-            </b-col>
-            <b-col sm="9">
-              <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
-            </b-col>
-          </b-row>
-        </b-container>
-      </div>
-
-      <div class="button-box">
-        <div>
-          <b-button variant="primary">Primary</b-button>
-          <b-button variant="secondary">Secondary</b-button>
-          <b-button variant="success">Success</b-button>
-          <b-button variant="danger">Danger</b-button>
-          <b-button variant="warning">Warning</b-button>
-          <b-button variant="info">Info</b-button>
-          <b-button variant="light">Light</b-button>
-          <b-button variant="dark">Dark</b-button>
+          <p class="mt-4">
+            Slide #: {{ slide }}<br />
+            Sliding: {{ sliding }}
+          </p>
         </div>
-      </div>
-    </b-container>
+
+        <div class="jumbotron-box">
+          <b-row>
+            <b-jumbotron>
+              <template v-slot:header>BootstrapVue</template>
+
+              <template v-slot:lead>
+                This is a simple hero unit, a simple jumbotron-style component
+                for calling extra attention to featured content or information.
+              </template>
+
+              <hr class="my-4" />
+
+              <p>
+                It uses utility classes for typography and spacing to space
+                content out within the larger container.
+              </p>
+
+              <b-button variant="primary" href="#">Do Something</b-button>
+              <b-button variant="success" href="#">Do Something Else</b-button>
+            </b-jumbotron>
+          </b-row>
+        </div>
+
+        <b-row>
+          <b-img
+            src="https://picsum.photos/1024/400/?image=41"
+            fluid
+            alt="Responsive image"
+          ></b-img>
+        </b-row>
+
+        <div class="form-box">
+          <b-container fluid>
+            <b-row class="my-1" v-for="type in types" :key="type">
+              <b-col sm="3">
+                <label :for="`type-${type}`"
+                  >Type <code>{{ type }}</code
+                  >:</label
+                >
+              </b-col>
+              <b-col sm="9">
+                <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
+              </b-col>
+            </b-row>
+          </b-container>
+        </div>
+
+        <div class="button-box">
+          <div>
+            <b-button variant="primary">Primary</b-button>
+            <b-button variant="secondary">Secondary</b-button>
+            <b-button variant="success">Success</b-button>
+            <b-button variant="danger">Danger</b-button>
+            <b-button variant="warning">Warning</b-button>
+            <b-button variant="info">Info</b-button>
+            <b-button variant="light">Light</b-button>
+            <b-button variant="dark">Dark</b-button>
+          </div>
+        </div>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -161,6 +162,7 @@ export default {
     return {
       slide: 0,
       sliding: null,
+      sticky: true,
       types: [
         "text",
         "number",
